75aa3c8a6dd4d3956b5c607f1788b1dde63d4543
timurboys112 <120889499+timurboys112@users.noreply.github.com>
2026-03-02 11:25:33 +0700

    Update MongoDB connection string and refactor server.js for improved readability

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const bookRoutes = require("./routes/books");
 
+const app = express();
+
+// Middleware
 app.use(express.json());
 
-// routes
+// Connect ke MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected 🚀"))
+  .catch(err => console.error("MongoDB Error:", err.message));
+
+// Routes
 app.use("/api/books", bookRoutes);
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(err.status || 500).json({
     error: err.message || "Internal Server Error",
   });
 });
-
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb://anindyabulan16_db_user:Moon1607@cluster0.raht8jf.mongodb.net/bookdb?retryWrites=true&w=majority&authSource=admin"
-)
-  .then(() => console.log("MongoDB Connected 🚀"))
-  .catch(err => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
